8045cbe7ec7ca7df5269f3f2c03c373e9c46fc62
thanhnga <phamthanhnga2005hn@gmail.com>
2025-03-26 00:07:07 +0700

    day danh muc len client

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['success_message'])) {
     unset($_SESSION['success_message']); // Xóa thông báo sau khi hiển thị
 }
 
-
+require_once 'controller/shopController.php';
 require_once 'controller/loginController.php';
 require_once 'controller/homeController.php';
 require_once 'controller/registerController.php';
@@ -20,6 +20,8 @@ require_once 'model/homeModel.php';
 require_once 'model/userModel.php';
 require_once 'model/orderModel.php';
 require_once 'commoms/function.php';
+require_once 'view/shop.php';
+require_once 'model/shopModel.php';
 
 $act = $_GET['act'] ?? '/';
 match ($act) {
@@ -29,6 +31,8 @@ match ($act) {
     'logout' => (new LogoutController())->logout(),
     'profile' => (new ProfileController())->profile(),
     'updateProfile' => (new ProfileController())->updateProfile(),
+    'shop' => (new shopController())->showShop(),
+    'shop_cat' => (new shopController())->cat_pro($_GET['id']),
     // 'order' => (new OrderController())->order(),
     default => (new HomeController())->home(),
 };
